0907b711d87ba3ab85f38016ab8dd75d71830ab5
王聪 <2441413514@qq.com>
2017-03-08 23:45:16 +0800

    Test代码

---
+++ src/com/lifeasst/test/Test.java
@@ -0,0 +1,14 @@
+package com.lifeasst.test;
+/** 
+* @author 王聪 E-mail: 2441413514@qq.com
+* @version 创建时间：2017年3月8日 下午11:44:52 
+* 
+*/
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
